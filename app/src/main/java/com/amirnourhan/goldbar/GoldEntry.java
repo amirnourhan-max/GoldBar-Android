aaaa67d0c878116dd55ccff9ecd884a1cfa0f10c
amirnourhan-max <amirnourhan@gmail.com>
2026-08-18 03:53:52 +0330

    Add GoldEntry model

---
+++ app/src/main/java/com/amirnourhan/goldbar/GoldEntry.java
@@ -0,0 +1,13 @@
+package com.amirnourhan.goldbar;
+
+public final class GoldEntry {
+    public int guide;
+    public double weight;
+    public double assay;
+
+    public GoldEntry(int guide, double weight, double assay) {
+        this.guide = guide;
+        this.weight = weight;
+        this.assay = assay;
+    }
+}
